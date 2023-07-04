cf71ed4796fb54f63c97caeb80bf67cd35488e96
heeba-khan <111532086+heeba-khan@users.noreply.github.com>
2023-07-04 22:15:33 +0530

    Time: 10 ms (51.31%), Space: 10 MB (32.35%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+
+	int singleNumber(vector<int>& nums) {
+	unordered_map<int,int> mp;
+        for(auto x: nums){
+            mp[x]++;
+        }
+        for(auto i=mp.begin(); i!=mp.end();i++){
+            if(i->second==1) return i->first;
+        }
+        return -1;
+    }
+};
+
+
