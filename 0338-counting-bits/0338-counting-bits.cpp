f00a973d618e6d37f6692b7760f0989683090305
Heeba Khan <heebak45@gmail.com>
2023-09-01 18:23:39 +0530

    Time: 4 ms (67.25%), Space: 8.5 MB (32.26%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+      vector<int>v;
+        
+        for(int i=0;i<=n;i++){
+            int sum=0,num=i;
+            
+            while(num!=0){
+                sum+=num%2;
+                num=num/2;
+            }
+            v.push_back(sum);
+        }
+        return v;
+    }
+};
