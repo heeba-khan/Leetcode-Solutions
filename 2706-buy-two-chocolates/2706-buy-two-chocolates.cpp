ab5e9e3161c85f5119da2d293dae9e6c41573aeb
Heeba Khan <heebak45@gmail.com>
2023-12-20 19:23:41 +0530

    Time: 16 ms (33.75%), Space: 47 MB (58.30%) - LeetHub

---
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int buyChoco(vector<int>& prices, int money) {
+        int n=prices.size();
+        sort(prices.begin(),prices.end());
+        int a=prices[0];
+        int b=prices[1];
+        
+        if(a+b<=money) return money-(a+b);
+        
+        else
+            return money;
+    }
+};
