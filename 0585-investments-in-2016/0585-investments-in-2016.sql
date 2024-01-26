defce78ffd46efe53c1fde0d44056ed9458eb0d6
Heeba Khan <heebak45@gmail.com>
2024-01-26 19:04:57 +0530

    Time: 875 ms (68.19%), Space: 0B (100.00%) - LeetHub

---
+++ 0585-investments-in-2016/0585-investments-in-2016.sql
@@ -0,0 +1,21 @@
+# Write your MySQL query statement below
+
+
+with cte as
+(select concat(lat,',',lon) as location
+from Insurance
+group by lat,lon
+having count(pid)>1),
+
+cte2 as
+(
+select distinct I1.*
+from Insurance as I1
+    left join Insurance as I2
+    on I1.tiv_2015=I2.tiv_2015
+    where I1.pid <> I2.pid
+    and concat(I1.lat,',',I1.lon) NOT IN (select location from cte)
+)
+
+select round(sum(tiv_2016),2) as tiv_2016
+from cte2
