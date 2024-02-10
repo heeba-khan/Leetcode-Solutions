d4ba37bedfa3919fcf31992580d20dabd0b7b20e
Heeba Khan <heebak45@gmail.com>
2024-02-10 11:22:32 +0530

    Time: 10 ms (45.78%), Space: 15.6 MB (26.47%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -12,8 +12,8 @@ public:
     }
     
     int countSubstrings(string s) {
-        int n=s.length();
         int cnt=0;
+        int n=s.length();
         
         for(int i=0;i<n;i++){
             int odd=f(s,i,i);
