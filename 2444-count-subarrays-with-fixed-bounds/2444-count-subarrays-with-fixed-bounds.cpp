0ed40bf7b6d46820cf95d799b8b7ae580f67b6d9
Heeba Khan <heebak45@gmail.com>
2024-04-01 03:35:15 +0530

    Time: 81 ms (87.89%), Space: 82.6 MB (54.90%) - LeetHub

--- 2444-count-subarrays-with-fixed-bounds/2444-count-subarrays-with-fixed-bounds.cpp
+++ 2444-count-subarrays-with-fixed-bounds/2444-count-subarrays-with-fixed-bounds.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
     long long countSubarrays(vector<int>& nums, int minK, int maxK) {
-        int n=nums.size();
-        int left=-1,minn=-1,maxx=-1;
-        long long cnt=0;
-        for(int i=0;i<n;i++){
-            if(nums[i]>=minK&&nums[i]<=maxK){
-                minn=(nums[i]==minK)?i:minn;
-                maxx=(nums[i]==maxK)?i:maxx;
-                cnt+= max(0, min(minn, maxx) - left);
+        long long res = 0;
+        int bad_idx = -1, left_idx = -1, right_idx = -1;
+
+        for (int i = 0; i < nums.size(); ++i) {
+            if (!(minK <= nums[i] && nums[i] <= maxK)) {
+                bad_idx = i;
             }
-            else
-            {
-                left=i;
-                minn=-1,maxx=-1;
+
+            if (nums[i] == minK) {
+                left_idx = i;
             }
+
+            if (nums[i] == maxK) {
+                right_idx = i;
+            }
+
+            res += max(0, min(left_idx, right_idx) - bad_idx);
         }
-        return cnt;
+
+        return res;
     }
 };
