54355488e73103db682f7ae01bfae0950a62156a
Heeba Khan <heebak45@gmail.com>
2024-04-06 01:59:08 +0530

    Time: 0 ms (100.00%), Space: 8.7 MB (16.41%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.cpp
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
     string makeGood(string s) {
-        if(s.size()==1) return s;
-        while(1){
         stack<char>st;
         
-        st.push(s[0]);
-        int index=0;
-        int n=s.size();
-        
-        for(int i=1;i<n;i++){
-            if(s[i]+32==st.top()||s[i]-32==st.top()){
-                s.erase(s.begin()+i);
-                s.erase(s.begin()+index);
+        for(char c:s){
+            if(!st.empty()&&abs(c-st.top())==32){
                 st.pop();
             }
-                index=i;
-                st.push(s[i]);
-            
+            else
+                st.push(c);
         }
-        if(n==s.size()) return s;
+        
+        string res;
+        while(!st.empty()){
+            res=st.top()+res;
+            st.pop();
         }
-        return "";
+        return res;
     }
 };
+
