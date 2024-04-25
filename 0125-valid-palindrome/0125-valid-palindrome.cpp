9c34a5b7ccdf7debc4ccbfcaf1944b6083020ab0
Heeba Khan <heebak45@gmail.com>
2024-04-25 11:06:46 +0530

    Time: 8 ms (34.85%), Space: 8.5 MB (97.25%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,39 +1,26 @@
 class Solution {
 public:
-//     string f(string &s){
-        
-//         string str="";
-//         for(int i=0;i<s.length();i++){
-//             if(s[i]>='A'&&s[i]<='Z')
-//                 str+=tolower(s[i]);
-//             else if(s[i]>='a'&&s[i]<='z')
-//                 str+=tolower(s[i]);
-//         }
-//                     return str;
-//     }
+   
     bool isPalindrome(string s) {
-        int n=s.length();
-        int i=0,j=n-1;
-        // string st=f(s);
+       int i=0;
+        int j=s.size()-1;
         
-      while(i<=j){
-          if(!isalnum(s[i])){
-              i++;
-              continue;
-          }
-          if(!isalnum(s[j])){
-              j--;
-              continue;
-          }
-          if(tolower(s[i])!=tolower(s[j])){
-              return false;
-          }
-          else{
-              i++;
-              j--;
-          }
-          
-      }
+        while(i<=j){
+            if(!isalnum(s[i]))
+                i++;
+            
+            else if(!isalnum(s[j]))
+                j--;
+            
+            else if(tolower(s[i])!=tolower(s[j]))
+                return false;
+            
+            
+            else{
+                i++;
+                j--;
+            }
+        }
         return true;
     }
 };
