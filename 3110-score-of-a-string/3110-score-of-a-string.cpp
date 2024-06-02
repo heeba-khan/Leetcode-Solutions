18b1e905917cd8fd8f92eebd543b29d819b8b115
Heeba Khan <heebak45@gmail.com>
2024-06-02 12:31:31 +0530

    Time: 5 ms (27.28%), Space: 7.9 MB (7.39%) - LeetHub

--- 3110-score-of-a-string/3110-score-of-a-string.cpp
+++ 3110-score-of-a-string/3110-score-of-a-string.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     int scoreOfString(string s) {
+        int n=s.length();
         int sum=0;
-        for(int i=0;i<s.size()-1;i++){
-            sum+=abs((s[i]-'0')-(s[i+1]-'0'));
+        for(int i=0;i<n-1;i++){
+            sum+=abs(s[i]-'0'-(s[i+1]-'0'));    
         }
         return sum;
     }
